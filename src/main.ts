15b6749406c5ac383a773d80c3287c2f55829fe5
elachcdv <elach@edu.cdv.pl>
2022-04-10 15:24:34 +0200

    interface for classes

--- src/main.ts
+++ src/main.ts
@@ -3,33 +3,60 @@ import {Author} from './model/author';
 import {Comment} from './model/comment';
 // import {Api} from './api';
 
-class Api {
+interface DataProvider {
+  getPosts(): Promise<Post[]>;
+  getAuthor(authorId: number): Promise<Author>;
+  getComments(postId: number): Promise<Comment[]>;
+}
+
+class Api implements DataProvider {
   postsSuffix: string = 'posts';
   constructor(public readonly apiUrl: string) {}
+  public getPosts(): Promise<Post[]> {
+    return this.getApiResponse(this.getPostsUrl());
+  }
+  public getAuthor(authorId: number): Promise<Author> {
+    const userUrl = `${this.apiUrl}/users/${authorId}`;
+    return this.getApiResponse(userUrl);
+  }
+  public getComments(postId: number): Promise<Comment[]> {
+    const postCommentsUrl = `${this.apiUrl}/comments?postId=${postId}`;
+    return this.getApiResponse(postCommentsUrl);
+  }
   // definicja metody
   public getPostsUrl(): string {
     return `${this.apiUrl}/${this.postsSuffix}`;
   }
+  // async getApiResponse(url: string): Promise<any> {
+  private async getApiResponse(url: string): Promise<any> {
+    const postsRequest: Promise<Response> = fetch(url);
+    const response: Response = await postsRequest;
+    const json: any = response.json();
+    return json;
+  }
 }
 
-const apiUrl: string = 'https://jsonplaceholder.typicode.com';
+const api = new Api('https://jsonplaceholder.typicode.com');
 
+const apiUrl: string = 'https://jsonplaceholder.typicode.com';
 const postsUrl: string = `${apiUrl}/posts`;
-const commentsUrl: string = `${apiUrl}/comments`;
+// const commentsUrl: string = `${apiUrl}/comments`;
 const usersUrl: string = `${apiUrl}/users`;
 
 // async function setAuthor(authorId: number): void {
 async function setAuthor(authorId: number) {
-  const userUrl = `${usersUrl}/${authorId}`;
-  const user: Author = await getApiResponse(userUrl);
+  //   const userUrl = `${usersUrl}/${authorId}`;
+  //   const user: Author = await getApiResponse(userUrl);
+  const user: Author = await api.getAuthor(authorId);
   const userElement = document.getElementById('author');
   userElement.classList.add('author');
   userElement.innerHTML = `<h3>${user.name} <small>(${user.email})</small></h3>`;
 }
 
 async function loadComments(postId: number) {
-  const postCommentsUrl = `${commentsUrl}?postId=${postId}`;
-  const comments: Comment[] = await getApiResponse(postCommentsUrl);
+  //   const postCommentsUrl = `${commentsUrl}?postId=${postId}`;
+  //   const comments: Comment[] = await getApiResponse(postCommentsUrl);
+  const comments: Comment[] = await api.getComments(postId);
   const commentsContainer = document.getElementById('comments');
   commentsContainer.innerHTML = '';
   for (const comment of comments) {
@@ -57,19 +84,21 @@ async function addListElement(post: Post): Promise<void> {
   listContainer.append(element);
 }
 
-async function getApiResponse(url: string) {
-  const postsRequest: Promise<Response> = fetch(url);
-  const response: Response = await postsRequest;
-  const json: any = response.json();
-  // const json: any = response.json(); // jeśli nie wiemy jaki typ ma zwrócić
-  return json;
-}
+// async function getApiResponse(url: string) {
+//   const postsRequest: Promise<Response> = fetch(url);
+//   const response: Response = await postsRequest;
+//   const json: any = response.json();
+//   // const json: any = response.json(); // jeśli nie wiemy jaki typ ma zwrócić
+//   return json;
+// }
 
 document.addEventListener('DOMContentLoaded', (): void => {
   const content = document.querySelector('#content');
 
   setTimeout((): void => {
-    getApiResponse(postsUrl)
+    // getApiResponse(postsUrl)
+    api
+      .getPosts()
       .then((posts: Post[]) => {
         content.innerHTML = 'Select post&hellip;';
 
