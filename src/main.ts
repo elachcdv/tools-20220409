e7097e2f8d690256314ad73bb3096752d46c9e96
elachcdv <elach@edu.cdv.pl>
2022-04-10 13:06:00 +0200

    typescript ready file

--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,25 @@ async function getApiResponse(url: string) {
   //   const json: any = response.json(); // jeśli nie wiemy jaki typ ma zwrócić
   return json;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const content = document.querySelector('#content');
+
+  setTimeout((): void => {
+    getApiResponse(postsUrl)
+      .then(posts => {
+        content.innerHTML = 'Select post&hellip;';
+
+        for (const post of posts) {
+          addListElement(post);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally((): void => {
+        const loader = document.querySelector('#spinner');
+        loader.remove();
+      });
+  }, 2000);
+});
